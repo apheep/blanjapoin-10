bd4f8c7556c75878c67f172d843e91f26d1a81eb
apheep <afifkaromi264@gmail.com>
2025-12-02 19:12:31 +0700

    done search

--- resources/views/merchant/entertain.blade.php
+++ resources/views/merchant/entertain.blade.php
@@ -1,7 +1,6 @@
 <section id="section-entertain" class="mt-10 md:mt-14 mb-10 md:mb-14">
     <div class="mb-4 md:mb-6 flex items-center justify-between">
-        <h2 class="text-2xl md:text-3xl font-black text-neutral-900">
-  Hiburan</h2>
+        <h2 class="text-2xl md:text-3xl font-black text-neutral-900">🎬​ Hiburan</h2>
     </div>
 
     @php
